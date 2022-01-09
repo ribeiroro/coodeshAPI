b0558dd865c13b4825afcac3bac3d491ada88a15
ribeirosro <ribeiro.ro@live.com>
2022-01-09 13:18:01 -0300

    First commit

--- Coodesh/Program.cs
+++ Coodesh/Program.cs
@@ -6,7 +6,7 @@ namespace Coodesh
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
+            Console.WriteLine("First commit!");
         }
     }
 }
